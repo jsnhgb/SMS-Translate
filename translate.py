2a756d8ab106fa8d03338876c5e8f583e80d88f0
Jason Higbee <jsn.hgb@gmail.com>
2013-01-19 20:02:59 +0000

    Cleaned up code for public release

--- translate.py
+++ translate.py
@@ -5,42 +5,32 @@ from twilio import twiml
 from twilio.rest import TwilioRestClient
 from apiclient.discovery import build
 from unidecode import unidecode
-import json
 
 # Declare and configure application
 app = Flask(__name__)
-app.config['ACCOUNT_SID'] = 'ACe00bbbd006337d3d53e3c84d0525dd46'
-app.config['AUTH_TOKEN'] = '0fab38df4e6f2b6321fcdc91774bacf6'
-app.config['TRANSLATE_KEY'] = 'AIzaSyCtwxiJRg35WTPdhLApleT6RKipMn78kqE'
-#app.config['SONYA_APP_SID'] = 'APdc1d4351cd866dedeb09ece15f2e8cb0'
-# app.config['SONYA_CALLER_ID'] = SONYA_CALLER_ID
+app.config['ACCOUNT_SID'] = 'key goes here'
+app.config['AUTH_TOKEN'] = 'key goes here'
+app.config['TRANSLATE_KEY'] = 'key goes here'
 
 
 @app.route('/')
 def index():
+    '''index path to make sure Flask app is up and running.'''
     question = ("hello twi")
     return str(question)
 
 
 @app.route('/sms', methods=['POST'])
 def sms():
+    '''Translate from english to french and vice versa'''
     r = twiml.Response()
     client = TwilioRestClient(app.config['ACCOUNT_SID'], app.config['AUTH_TOKEN'])
     service = build('translate', 'v2', developerKey=app.config['TRANSLATE_KEY'])
     body = request.form['Body']
-#    if body.find('*') == '#':
-#        lang = body[1:2]
-#        query = body[4:]
-#    else:
-#        lang = 'fr'
-#        query = body
     gtrans = service.translations().list(target='fr', format='text', q=body).execute()
     if gtrans['translations'][0]['detectedSourceLanguage'] != 'en':
         #if lang not english then translate that into english
         gtrans = service.translations().list(target='en', format='text', q=body).execute()
-    #use json.dumps to get unicode and not ascii then reload to dict using loads
-    # translation = json.dumps(gtrans, ensure_ascii=False)
-   # translation_dict = json.loads(translation)
     r = client.sms.messages.create(to=request.form['From'], from_="+19177461980",
                                    body=unidecode(gtrans['translations'][0]['translatedText']))
     return str(r)
